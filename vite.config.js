c7903d587354c42f937bb99b9cd474efd3928de1
TheShivam114 <shivam.ml114@gmail.com>
2026-07-13 20:32:56 +0530

    correct vite.confit.js and create an image in the docker

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [
+      "next-gen-pharma-alb-189538282.ap-south-1.elb.amazonaws.com",
+    ],
+  },
+});
